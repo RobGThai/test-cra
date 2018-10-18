50f5894c2628aa3e07830ba5698eff02937a45c0
Newcomer <newcomer@COH000232.local>
2018-10-18 10:16:25 +0700

    Feature 2

--- src/components/label.js
+++ src/components/label.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Label extends Component {
     render() {
         return (
-            <div>Feature 1</div>
+            <div>Feature 2</div>
         );
     }
 };
